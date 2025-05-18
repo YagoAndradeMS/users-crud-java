c22cb49544fd713173978952c44c5ba072092b2c
Yago <andradeyago56@gmail.com>
2025-05-18 19:54:29 -0300

    Adding modal to exclude users

---
+++ frontend/src/components/modals/deleteUserModal/index.tsx
@@ -0,0 +1,63 @@
+import { X } from 'lucide-react';
+import api from '@/shared/services/api';
+
+interface DeleteUserModalProps {
+  userId: number;
+  onClose: () => void;
+  onDeleteSuccess?: () => void; // Callback opcional
+}
+
+export const DeleteUserModal = ({
+  userId,
+  onClose,
+  onDeleteSuccess,
+}: DeleteUserModalProps) => {
+  const handleDelete = async () => {
+    try {
+      await api.delete(`/users/${userId}`);
+      onDeleteSuccess?.(); // Executa callback se houver
+      onClose();
+    } catch (error) {
+      console.error('Erro ao excluir usuário:', error);
+    }
+  };
+
+  return (
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4'>
+      <div className='relative w-full max-w-md rounded-2xl bg-white p-6 shadow-xl'>
+        {/* Botão de Fechar */}
+        <button
+          onClick={onClose}
+          className='absolute right-4 top-4 text-gray-500 hover:text-red-500 transition-colors'
+        >
+          <X size={24} />
+        </button>
+
+        <h2 className='mb-4 text-xl font-semibold text-zinc-800'>
+          Confirmar exclusão
+        </h2>
+
+        <p className='text-sm text-gray-700 text-wrap'>
+          Tem certeza que deseja excluir este usuário? Esta ação não poderá ser
+          desfeita.
+        </p>
+
+        {/* Botões */}
+        <div className='mt-6 flex justify-end gap-4'>
+          <button
+            onClick={onClose}
+            className='rounded-md border border-gray-300 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition'
+          >
+            Cancelar
+          </button>
+          <button
+            onClick={handleDelete}
+            className='rounded-md bg-red-600 px-4 py-2 text-sm text-white hover:bg-red-700 transition'
+          >
+            Excluir
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/components/userTable/actions/index.tsx
+++ frontend/src/components/userTable/actions/index.tsx
@@ -1,9 +1,10 @@
 import { User } from '@/shared/types/User';
-import { FaEye, FaTrash } from 'react-icons/fa';
+import { FaTrash } from 'react-icons/fa';
 import { Icon } from './icon';
 import { FaPencil } from 'react-icons/fa6';
 import { useState } from 'react';
 import { CreateUserModal } from '@/components/modals/createOrEditUserModal';
+import { DeleteUserModal } from '@/components/modals/deleteUserModal';
 
 interface ActionsProps {
   user: User;
@@ -11,6 +12,7 @@ interface ActionsProps {
 
 export const Actions = ({ user }: ActionsProps) => {
   const [openEditModalUser, setOpenEditModalUser] = useState<boolean>(false);
+  const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
   return (
     <div className='flex flex-row justify-end items-center gap-4'>
       {/* <Icon
@@ -27,7 +29,7 @@ export const Actions = ({ user }: ActionsProps) => {
       />
       <Icon
         classname='bg-red-200 hover:bg-red-300'
-        execute={() => console.log()}
+        execute={() => setShowDeleteModal(true)}
         icon={<FaTrash className='text-red-600 hover:text-red-700' />}
       />
 
@@ -35,6 +37,14 @@ export const Actions = ({ user }: ActionsProps) => {
         <CreateUserModal
           userToEdit={user}
           onClose={() => setOpenEditModalUser(false)}
+        />
+      )}
+
+      {showDeleteModal && (
+        <DeleteUserModal
+          userId={user.id}
+          onClose={() => setShowDeleteModal(false)}
+          // onDeleteSuccess={() => fetchUsersAgain()} // Exemplo de callback para atualizar a lista
         />
       )}
     </div>
